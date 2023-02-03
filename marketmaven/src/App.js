e5c464865fbb0e8b91624ca88eed3dd0213f0884
Anuj Contractor <63806427+anujcontractor@users.noreply.github.com>
2023-02-03 23:24:05 +0530

    make navbar

--- marketmaven/src/App.js
+++ marketmaven/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Login from './Pages/Login';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Dashboard from './Pages/Dashboard';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      </div>
+    <>
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </HashRouter>
+  </>
   );
 }
 
