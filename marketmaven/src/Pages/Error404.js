fbd81ed3b45b653c732b8917d9e900e97ee1048e
Govrani Keyur <202101498@daiict.ac.in>
2023-02-04 01:14:08 +0530

    make error page

--- marketmaven/src/Pages/Error404.js
+++ marketmaven/src/Pages/Error404.js
@@ -1,30 +1,25 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
-import '@fontsource/montserrat'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/montserrat';
+import '@fontsource/lato'
 
 export const Error404 = () => {
-    // const [variant, setVariant] = React.useState('solid');
     return (
-        <div className="error404" style={{fontFamily: "Montserrat"}}>
+        <div className="error404">
             <div>
-                <div style={{marginTop: "50px", marginLeft: "25px"}}>
-                    {/* <h1 className="h1">Error 404!</h1> */}
-                    <Typography variant="h1" style={{color: "red"}}>
+                <div style={{ marginLeft: "25px"}}>
+                    <Typography variant="h1" style={{color: "red", fontFamily: "Lato"}}>
                         Error 404!
                     </Typography>
-                    {/* <h2 className="">We can't find the page you're looking for.</h2> */}
-                    <Typography variant="h5" gutterBottom style={{marginLeft: "8px"}}>
+                    <Typography variant="h5" gutterBottom style={{marginLeft: "8px", fontFamily: "Montserrat"}}>
                         We can't find the page you're looking for.
                     </Typography>
-                    {/* <div className="d-flex justify-content-start">
-                        <a href="#/" className="btn btn-light mt-2" style={{fontSize: "18px"}}>Go Home</a>
-                    </div> */}
-                    <Button size="large" variant="contained" style={{marginLeft: "10px", marginTop: "5px", color: "white"}}>Go Home</Button>
+                    <Button size="large" variant="contained" style={{marginLeft: "10px", marginTop: "5px", color: "white", fontFamily: "Montserrat"}}>Go Home</Button>
                 </div>
                 </div>
         </div>
