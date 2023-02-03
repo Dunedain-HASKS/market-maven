6a55fe2b0d4f0061a262bd1c183b0bf6f8afb9b2
ShivangKacha <shivangkachasbk9@gmail.com>
2023-02-04 04:00:33 +0530

    news comapny

--- marketmaven/src/App.js
+++ marketmaven/src/App.js
@@ -7,7 +7,7 @@ import { Error404 } from "./Pages/Error404";
 import Profile from "./Pages/Profile";
 import SignUp from "./Pages/SignUp";
 import Companies from "./Pages/Companies";
-
+import News from "./Pages/News";
 function App() {
   return (
     <>
@@ -17,8 +17,9 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<DashBoard />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/companies" element={<Companies/>} />
-          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </HashRouter>
